211000d6091a0207a876757e00e45e6091eaaaa7
Paulo Cidrão <paulocdrao.123@gmail.com>
2025-05-03 23:39:43 -0300

    feat:create routes and error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout name="default">
+    <section class="min-h-[80vh] flex flex-col items-center justify-center">
+      <div class="text-center space-y-4">
+        <h2 class="text-8xl font-bold">404</h2>
+        <h1 class="text-3xl">Página não encontrada!</h1>
+        <p class="mt-4">A página que você está procurando não existe.</p>
+        <NuxtLink
+          to="/"
+          class="inline-block mt-6 px-6 py-3 bg-white text-black rounded-lg hover:bg-gray-200 transition-colors"
+        >
+          Voltar para página inicial
+        </NuxtLink>
+      </div>
+    </section>
+  </NuxtLayout>
+</template>
